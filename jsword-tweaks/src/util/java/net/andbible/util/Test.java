2c33bb36202d9ca18e2df139b2d9091b3f41b038
Martin Denham <mjdenham@gmail.com>
2013-08-02 21:36:18 +0100

    refactor lots of jsword tweaks into js and ab

--- jsword-tweaks/src/util/java/net/andbible/util/Test.java
+++ jsword-tweaks/src/util/java/net/andbible/util/Test.java
@@ -2,14 +2,14 @@ package net.andbible.util;
 
 import java.util.Locale;
 
-import junit.framework.Assert;
 
 import org.crosswire.common.util.CallContext;
 import org.crosswire.jsword.JSMsg;
 import org.crosswire.jsword.book.Book;
 import org.crosswire.jsword.book.BookException;
 import org.crosswire.jsword.book.Books;
-import org.crosswire.jsword.passage.Key;
+import org.crosswire.jsword.passage.*;
+import org.crosswire.jsword.versification.system.Versifications;
 
 public class Test {
 
@@ -18,39 +18,54 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		
-		
-		new Test().testInvalidKey();
-		new Test().testCallContext();
-		
-		Locale.setDefault(Locale.FRENCH);
-		String jobName = JSMsg.gettext("Creating index. Processing {0}", "ABC");
-		System.out.println(jobName);
-		
-		Locale.setDefault(Locale.GERMAN);
-		String msg = JSMsg.gettext("No entry for '{0}' in {1}.", "ONE", "TWO");
-		System.out.println(msg);
+       new Test().testObadiah();
+//		new Test().testInvalidKey();
+//		new Test().testCallContext();
+//
+//		Locale.setDefault(Locale.FRENCH);
+//		String jobName = JSMsg.gettext("Creating index. Processing {0}", "ABC");
+//		System.out.println(jobName);
+//
+//		Locale.setDefault(Locale.GERMAN);
+//		String msg = JSMsg.gettext("No entry for '{0}' in {1}.", "ONE", "TWO");
+//		System.out.println(msg);
 		
 
 	}
 
-    public void testInvalidKey() {
-        Book book = Books.installed().getBook("Pilgrim");
-        if (book != null) {
-            Key key = book.getGlobalKeyList();
-            try {
-                book.getRawText(key);
-            } catch (NullPointerException e) {
-                Assert.fail("test for bad key should not have thrown an NPE.");
-            } catch (BookException e) {
-            	System.out.println(e.getMessage());
-                Assert.assertEquals("testing for a bad key", "No entry for '' in Pilgrim.", e.getMessage());
-            }
+    public void testObadiah() {
+        try {
+            Key obd = PassageKeyFactory.instance().getKey(Versifications.instance().getVersification("KJV"), "Obd");
+            System.out.println(obd);
+            Verse verse = KeyUtil.getVerse(obd);
+            System.out.println(verse);
+
+            Key obd1 = PassageKeyFactory.instance().getKey(Versifications.instance().getVersification("KJV"), "Obd 1");
+            System.out.println(obd1);
+            Verse verse1 = KeyUtil.getVerse(obd1);
+            System.out.println(verse1);
+        } catch (NoSuchKeyException e) {
+            e.printStackTrace();
         }
     }
-    
-    public void testCallContext() {
-		System.out.println(CallContext.getCallingClass());
-		Assert.assertEquals("net.andbible.util.Test",CallContext.getCallingClass().getName());
-    }
+//    public void testInvalidKey() {
+//        Book book = Books.installed().getBook("Pilgrim");
+//        if (book != null) {
+//            Key key = book.getGlobalKeyList();
+//            try {
+//                book.getRawText(key);
+//            } catch (NullPointerException e) {
+//                Assert.fail("test for bad key should not have thrown an NPE.");
+//            } catch (BookException e) {
+//            	System.out.println(e.getMessage());
+//                Assert.assertEquals("testing for a bad key", "No entry for '' in Pilgrim.", e.getMessage());
+//            }
+//        }
+//    }
+//
+//    public void testCallContext() {
+//		System.out.println(CallContext.getCallingClass());
+//		Assert.assertEquals("net.andbible.util.Test",CallContext.getCallingClass().getName());
+//    }
 
 }
